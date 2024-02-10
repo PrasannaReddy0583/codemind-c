d3aa31efee9799286d1461c38486e1c937d3a2f1
Prasanna Reddy Nallamilli <155651111+PrasannaReddy0583@users.noreply.github.com>
2024-02-10 15:27:01 +0530

    #include<stdio.h> int main() {     int a,i,k;     scanf("%d",&a);     for(i=1;i<=a;i++)     {     for(k=1;k<=a;k++)         {             printf("%d ",k);         }     printf(" ");     } }

---
+++ Basic_Pattern_1.2.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int a,i,k;
+    scanf("%d",&a);
+    for(i=1;i<=a;i++)
+    {
+    for(k=1;k<=a;k++)
+        {
+            printf("%d ",k);
+        }
+    printf("
+");
+    }
+}
