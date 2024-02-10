2b28bb0dc41a609197f0af9e93d31df049271929
Prasanna Reddy Nallamilli <155651111+PrasannaReddy0583@users.noreply.github.com>
2024-02-10 15:24:55 +0530

    #include<stdio.h> int main() {     int a,i,k;     scanf("%d",&a);     for(i=0;i<a;i++)     {         for(k=0;k<a;k++)         {             printf("* ");         }     printf(" ");     } }

---
+++ Basic_Pattern_1.1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int a,i,k;
+    scanf("%d",&a);
+    for(i=0;i<a;i++)
+    {
+        for(k=0;k<a;k++)
+        {
+            printf("* ");
+        }
+    printf("
+");
+    }
+}
